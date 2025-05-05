756cf41a08ef1aaaea517d4bedd1ed52576226df
AhzamSyed <ahzamsyed6@gmail.com>
2025-05-06 00:43:55 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About</title>
+</head>
+<body>
+  <?php
+  include "header.php";
+  ?>
+  <h1>About Us</h1>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, neque. Neque error totam dolorem a molestiae corporis ratione! Quis, dignissimos earum? Enim harum provident saepe deleniti adipisci laboriosam ipsa optio!</div>
+  <?php
+  include "footer.php";
+  ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOME</title>
+</head>
+<body>
+  <?php
+  include "header.php";
+  ?>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, neque. Neque error totam dolorem a molestiae corporis ratione! Quis, dignissimos earum? Enim harum provident saepe deleniti adipisci laboriosam ipsa optio!</div>
+  <?php
+  include "footer.php";
+  ?>
+</body>
+</html>
