2ba82533d7cd76364d5d81e84b11d91a28f24c6e
Syed Ahzam Hussain <ahzamsyed6@gmail.com>
2025-05-08 13:02:51 +0500

    session

--- session.php
+++ session.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session1</title>
+</head>
+<body>
+<center>
 <?php
 
 $user = $_POST['txtuser'];
 $pass = $_POST['txtpass'];
-
-
+echo "Username :".$user;
+echo "<br>Password :".$pass;
 
 ?>
+</center>
+</body>
+</html>
